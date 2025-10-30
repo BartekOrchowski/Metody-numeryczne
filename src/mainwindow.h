f68a174816c8e9078d4f46930edc67f27d751bc6
KarolFra <KarolFra@github.com>
2025-10-30 19:11:59 +0100

    commit add windows implementation

---
+++ src/mainwindow.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onLoadCSV();
+    void onFitModel();
+
+private:
+    Ui::MainWindow *ui;
+};
