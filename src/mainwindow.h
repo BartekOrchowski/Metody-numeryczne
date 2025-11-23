a056cc0c50a362cfe796b3c17e8a3e6e2313c70a
KarolFra <KarolFra@github.com>
2025-11-23 18:33:34 +0100

    Update diode data and docs

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -16,27 +16,13 @@ public:
 private slots:
     void onLoadCSV();
     void onFitModel();
-    void resetZoom();
 
 private:
-    enum class Method {
-        Analytical = 0,
-        FiniteDifference,
-        Newton
-    };
-
-    Method currentMethod() const;
-
     Ui::MainWindow *ui;
     QVector<QPointF> m_data;
-    QVector<QPointF> m_analyticalData;
-    QVector<QPointF> m_fdmData;
-    QVector<QPointF> m_newtonData;
+    QVector<QPointF> m_modelData;
 
     QVector<QPointF> loadCSV(const QString &filePath);
-    QVector<QPointF> generateAnalyticalCurve(int samples) const;
-    QVector<QPointF> generateFiniteDifferenceCurve(int samples) const;
-    QVector<QPointF> generateNewtonCurve(int samples) const;
-    double solveNewtonCurrent(double voltage, double saturationCurrent, double idealityFactor, double thermalVoltage, double seriesResistance) const;
+    QVector<QPointF> generateShockleyCurve() const;
     void updateChart();
 };
